dbb3ccd7ea29915e8aedb0f35fb684a253f958c4
Allon Mureinik <amureini@redhat.com>
2017-05-09 00:48:59 +0300

    Initial (empty) DBUnitTransformer

---
+++ src/main/java/org/dbunit/contrib/DBUnitTransformer.java
@@ -0,0 +1,9 @@
+package org.dbunit.contrib;
+
+/**
+ * A utility class to transform a DBUnit XML file to another format.
+ *
+ * @author Allon Mureinik (amureini@redhat.com)
+ */
+public class DBUnitTransformer {
+}
