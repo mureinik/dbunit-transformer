3135767d542b4b57178e710c51e6764e77f6212b
Allon Mureinik <amureini@redhat.com>
2017-05-09 11:09:11 +0300

    Add command line arguments parsing

---
+++ src/main/java/org/dbunit/contrib/DBUnitTransformerExitCodes.java
@@ -0,0 +1,30 @@
+package org.dbunit.contrib;
+
+/**
+ * Exit codes for the DBUnit Transformer program.
+ *
+ * @author Allon Mureinik (amureini@redhat.com)
+ */
+public enum DBUnitTransformerExitCodes {
+    OK(0),
+    ERROR_ARGS(1),
+    ERROR_INFILE(2);
+
+    private int rc;
+
+    DBUnitTransformerExitCodes(int rc) {
+        this.rc = rc;
+    }
+
+    public int getExitCode() {
+        return rc;
+    }
+
+    public boolean isOK() {
+        return this == OK;
+    }
+
+    public boolean isError() {
+        return !isOK();
+    }
+}
